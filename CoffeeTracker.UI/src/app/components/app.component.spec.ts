48a42c78c0d9202403500370ce6c74692e63abb5
BBualdo <mrseb10@gmail.com>
2024-05-05 19:18:38 +0200

    Main Menu template

--- CoffeeTracker.UI/src/app/components/app.component.spec.ts
+++ CoffeeTracker.UI/src/app/components/app.component.spec.ts
@@ -17,13 +17,14 @@ describe('AppComponent', () => {
   it(`should have the 'CoffeeTracker.UI' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CoffeeTracker.UI');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CoffeeTracker.UI');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, CoffeeTracker.UI'
+    );
   });
 });
